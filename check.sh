297c9aa9e1ff6eb7555ddeddae24f3139ddfe1ad
mmaureliooo <marcocabraldepablo@gmail.com>
2026-02-27 08:37:53 +0100

    elimino archivos del laboratorio

--- check.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-function check(){
-	if [ -e "$1" ]
-		then
-		return 0
-	else
-		return 1
-	fi
-}
--- comprobacion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/marco/code/mmaureliooo/ssoo/check.sh
-echo "introduzca el nombre del archivo" ;read x
-if check $x
-then 
-echo "$x existe !"
-else
-echo "$x no existe"
-fi
