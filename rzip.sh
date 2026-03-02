f25dd12c803c4d86b4a9514a9fffedb2e760eb4a
MarioGayarr <9407075@alumnos.ufv.es>
2026-03-02 08:46:03 +0100

    rzip

---
+++ rzip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+read nombre_archivo
+read compresion
+
+if [ -f "$nombre_archivo" ]; then
+    if [[ "$compresion" == "tar" ]]; then
+        tar -czfv "${nombre_archivo}.tar" "$nombre_archivo"
+        exit 1
+    elif [[ "$compresion" == "zip" ]]; then   
+        zip -r "${nombre_archivo}.zip" "$nombre_archivo"
+        exit 1
+    else
+        echo "Opcion de compresion invalida"
+        exit 0
+    fi
+    
+else
+    echo "No se ha encontrado $nombre_archivo"
+    exit 0
+fi
+
+
+
+
+
