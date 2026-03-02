a59a7c303b45085eea01437ca209aea7664c818e
MarioGayarr <9407075@alumnos.ufv.es>
2026-03-02 09:06:21 +0100

    rzip niquel

--- rzip.sh
+++ rzip.sh
@@ -5,14 +5,17 @@ read compresion
 if [ -f "$nombre_archivo" ]; then
     if [[ "$compresion" == "tar" ]]; then
         tar -czfv "${nombre_archivo}.tar" "$nombre_archivo"
-        exit 1
+        
     elif [[ "$compresion" == "zip" ]]; then   
         zip -r "${nombre_archivo}.zip" "$nombre_archivo"
-        exit 1
+        
     else
         echo "Opcion de compresion invalida"
         exit 0
     fi
+
+    rm "$nombre_archivo"
+    exit 1
     
 else
     echo "No se ha encontrado $nombre_archivo"
